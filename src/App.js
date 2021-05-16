70b543354714aefccd63ee024d48dbb5df6f559b
mi4en <marionikolov87@gmail.com>
2021-05-16 09:39:26 +0300

    implement cart and car itme components with styles;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 import { commerce } from './lib/commerce'
-import { Products, Navbar } from './components'
+import { Products, Navbar, Cart } from './components'
 
 const App = () => {
 	const [products, setProducts] = useState([])
@@ -21,7 +21,6 @@ const App = () => {
 		const res = await commerce.cart.add(productId, quantity)
 
 		setCart(res.cart)
-		console.log(cart)
 	}
 
 	useEffect(() => {
@@ -29,12 +28,11 @@ const App = () => {
 		fetchCart()
 	}, [])
 
-	console.log(cart)
-
 	return (
 		<div>
 			<Navbar totalItems={cart.total_items} />
-			<Products products={products} onAddToCart={handleAddToCart} />
+			{/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+			<Cart cart={cart} />
 		</div>
 	)
 }
---
+++ src/components/cart/Cart.jsx
@@ -0,0 +1,68 @@
+import React, { Fragment } from 'react'
+import { Container, Typography, Button, Grid } from '@material-ui/core'
+
+import useStyles from './styles'
+import CardItem from './cartItem/CartItem'
+
+const Cart = ({ cart }) => {
+	const classes = useStyles()
+
+	const EmptyCart = () => (
+		<Typography variant='subtitle1'>
+			You have no items in your shopping cart, start adding some!
+		</Typography>
+	)
+
+	const FilledCart = () => (
+		<Fragment>
+			<Grid container spacing={3}>
+				{cart.line_items.map(item => (
+					<Grid item xs={12} sm={4} key={item.id}>
+						<CardItem item={item} />
+					</Grid>
+				))}
+			</Grid>
+
+			<div className={classes.details}>
+				<Typography variant='h4'>
+					Subtotal: {cart.subtotal.formatted_with_symbol}
+				</Typography>
+				<div>
+					<Button
+						className={classes.emptyBtn}
+						size='large'
+						type='button'
+						variant='contained'
+						color='secondary'
+					>
+						Empty Cart
+					</Button>
+					<Button
+						className={classes.checkoutBtn}
+						size='large'
+						type='button'
+						variant='contained'
+						color='primary'
+					>
+						Checkout
+					</Button>
+				</div>
+			</div>
+		</Fragment>
+	)
+
+	if (!cart.line_items) return 'Loading...'
+
+	return (
+		<Container>
+			<div className={classes.toolbar} />
+			<Typography className={classes.title} variant='h3' gutterBottom>
+				Your Shopping Cart
+			</Typography>
+
+			{!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+		</Container>
+	)
+}
+
+export default Cart
